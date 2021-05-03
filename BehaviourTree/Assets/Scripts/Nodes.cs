2f6de5832c4136aeb7c4b0639537b0d96e5a39f6
elhajjam.abdallah1@gmail.com <elhajjam.abdallah1@gmail.com>
2021-05-03 11:27:49 +0200

    Commit Nodes/Behaviour Tree

---
+++ BehaviourTree/Assets/Scripts/Nodes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+enum states
+{
+    NotExecuted,
+    Running,
+    Failure,
+    Success
+}
+
+
+public class Nodes
+{
+    //This is the actual state of the node
+    private states _state = states.NotExecuted;
+}
+
+public class Repeat : Nodes
+{
+    
+}

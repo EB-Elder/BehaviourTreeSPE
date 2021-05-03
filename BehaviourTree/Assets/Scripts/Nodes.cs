417a89ead80bce3977ddd87f484ba1cfca8adfeb
elhajjam.abdallah1@gmail.com <elhajjam.abdallah1@gmail.com>
2021-05-03 13:03:02 +0200

    Nodes Action Condition Selector done !

--- BehaviourTree/Assets/Scripts/Nodes.cs
+++ BehaviourTree/Assets/Scripts/Nodes.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-enum states
+public enum states
 {
     NotExecuted,
     Running,
@@ -12,14 +12,74 @@ enum states
     Success
 }
 
+public enum baseNodeType
+{
+    Condition,
+    Action
+}
+
 
 public class Nodes
 {
     //This is the actual state of the node
-    private states _state = states.NotExecuted;
+    
+    private Func<states> _action;
+    private baseNodeType _nodeType;
+    
+    public states state = states.NotExecuted;
+
+
+    protected Nodes()
+    {
+        
+    }
+    public Nodes(Func<states> f, baseNodeType newNodeType)
+    {
+        _action = f;
+        _nodeType = newNodeType;
+    }
+    
+    public virtual states Execute()
+    {
+        state = _action();
+        return state;
+    }
+    
 }
 
-public class Repeat : Nodes
+public class Selector : Nodes
 {
     
+    //INCOMPLET
+    private List<Nodes> nodeList = new List<Nodes>();
+
+    public void AddNode(ref Nodes newNode)
+    {
+        nodeList.Add(newNode);
+    }
+
+    public void ClearList()
+    {
+        nodeList.Clear();
+    }
+    
+    public override states Execute()
+    {
+        state = states.Failure;
+        foreach (Nodes node in nodeList)
+        {
+            state = node.Execute();
+            if (state == states.Success)
+            {
+                break;
+            }
+        }
+        return state;
+    }
+
+}
+
+public class Sequence : Nodes
+{
+    private List<Nodes> nodeList;
 }
---
+++ BehaviourTree/Assets/Scripts/TestingScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+
+    public states printHi()
+    {
+        return states.Success;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Nodes myAction = new Nodes(printHi, baseNodeType.Action);
+
+
+        if (myAction.Execute() == states.Success)
+            print("SUCCES !");
+
+    }
+}
